b56eb96622ac0f18188b69cc8823bfac183ef480
Robert Povelones <rpovelones@walkwest.com>
2021-10-19 14:19:04 -0400

    Downgrade gulp and add JS task and CSS minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,38 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var concat = require('gulp-concat');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    minify = require('gulp-minify');
 
+/**
+ * Sass task
+ */
 gulp.task('sass', function () {
     return gulp.src('./resources/sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
         .pipe(gulp.dest('./dist/css'));
 });
 
+/**
+ * JS task
+ */
+gulp.task('js', function () {
+    return gulp.src('./resources/js/**/*.js')
+        .pipe(minify())
+        .pipe(gulp.dest('./dist/js'));
+});
+
+/**
+ * Watch task
+ */
 gulp.task('watch', function () {
     gulp.watch('./resources/sass/**/*.scss', gulp.series('sass'));
 });
+
+/**
+ * Default task
+ */
+gulp.task('default', ['sass', 'js']);
